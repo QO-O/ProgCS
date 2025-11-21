0a79cd4ae8b7da65987e17e2e5e28a4f6b9bd00a
QO-O <39372@ma-web.nl>
2025-11-21 14:30:05 +0100

    02 finished

--- 01 functions/Program.cs
+++ 01 functions/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace _01_functions;
 
-class Program
+internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new Program();
+        program.Run();
+        //a function about running, a string because it's text
+        //it's a program, and the name is program
+        //I assume the return would be either a string or a bool
+    }
+    internal void Run()
+    {
+        Console.WriteLine("This is now the start of my program")
     }
 }
