232f48817d94705f2a165082f60f77e8eb2076c3
QO-O <39372@ma-web.nl>
2026-01-26 10:34:58 +0100

    03 done of 04

--- 04 lists/ForHerhaling/Lists shit/lists.cs
+++ 04 lists/ForHerhaling/Lists shit/lists.cs
@@ -1,3 +1,6 @@
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
 namespace Lists_shit;
 
 class Program
@@ -6,32 +9,34 @@ class Program
     {
         Program program = new Program();
         program.Run();
+        program.RemoveInLoop();
+    
 
-        string[] characters = new string[]
-        {
-            "Pac-Man","Samus","Crash Bandicoot","Spyro the Dragon","Donkey Kong","Mario","Luigi",
-            "Astro","Solid Snake","Master Chief","Kratos","Sonic the Hedgehog","Link"
-        };
+        // string[] characters = new string[]
+        // {
+        //     "Pac-Man","Samus","Crash Bandicoot","Spyro the Dragon","Donkey Kong","Mario","Luigi",
+        //     "Astro","Solid Snake","Master Chief","Kratos","Sonic the Hedgehog","Link"
+        // };
 
-        List<string> characterList = new List<string>();
+        // List<string> characterList = new List<string>();
 
-        characterList.Add("ELEPHANT PEACH");
+        // characterList.Add("ELEPHANT PEACH");
 
-        for (int i = 0; i < characters.Length; i++)
-        {
-            characterList.Add(characters[i]);
-        }
+        // for (int i = 0; i < characters.Length; i++)
+        // {
+        //     characterList.Add(characters[i]);
+        // }
 
     
-        for (int i = 0; i < characterList.Count; i++)
-        {
-            Console.WriteLine(characterList[i]);
-        }
+        // for (int i = 0; i < characterList.Count; i++)
+        // {
+        //     Console.WriteLine(characterList[i]);
+        // }
 
-        foreach(string character in characterList)
-        {
-            Console.WriteLine(character);
-        }
+        // foreach(string character in characterList)
+        // {
+        //     Console.WriteLine(character);
+        // }
 
        
     }
@@ -50,8 +55,46 @@ class Program
             Console.WriteLine(review);
         }
     }
+    internal void RemoveInLoop()
+    {
+        List<Pickup> pickups = new List<Pickup>();
+
+       for (int i = 0; i < 10; i++)
+        {
+            Pickup pickup = new Pickup()
+            {
+                x = i,
+                y = 0
+            };
+            pickups.Add(pickup);
+        }
+
+
+        // foreach (Pickup pickup in Pickups)
+        // {
+        //     if(pickup.x)
+        // ;}
+       for (int i = pickups.Count - 1; i > -1; i--)
+        {
+            Pickup pickup = pickups[i];
+           if (pickup.x == 4)
+            {
+                pickups.Remove(pickup);
+                Console.WriteLine($"{i}: picked up");
+            }
+            else
+            {
+                Console.WriteLine($"{i}: still here");
+            }
+        }
+    }
+
     internal void Run()
     {
         
     }
 }
+class Pickup
+    {
+        internal int x, y;
+    }
